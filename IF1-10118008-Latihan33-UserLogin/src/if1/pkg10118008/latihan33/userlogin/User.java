e2dbfc873bfd0f453b926dd1a2605368fb647b22
Nagaku <45015676+Nagaku@users.noreply.github.com>
2019-10-17 19:19:19 +0700

    Add files via upload

---
+++ IF1-10118008-Latihan33-UserLogin/src/if1/pkg10118008/latihan33/userlogin/User.java
@@ -0,0 +1,35 @@
+package if1.pkg10118008.latihan33.userlogin;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author ASUS
+ */
+public class User {
+	final private String username;
+	final private String password;
+	private boolean statusAkun;
+
+	private boolean cekAkun(String parUserName, String parPassword){
+		return this.username.equals(parUserName) && this.password.equals(parPassword);
+	}
+	private void hasilLogin(boolean status, String parUserName){
+		if(status)
+			System.out.printf("\n******HALLO %s******\nSelamat Datang di Aplikasi ini\n", parUserName.toUpperCase());
+		else
+			System.out.printf("\nOoops, Username atau Password anda salah\n");
+	}
+	public void pengecekkanLogin(String parUserName, String parPassword){
+		statusAkun = cekAkun(parUserName, parPassword);
+		hasilLogin(statusAkun, parUserName);	
+	}
+	public User(String username, String password){
+		this.username = username;
+		this.password = password;
+	}
+}
